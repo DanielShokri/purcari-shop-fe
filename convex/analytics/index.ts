1e11eed25401ec059a4a8a73ab7d64f647ba3e4d
danielshmuel-mirshukri <danielshmuel.mirshukri@justeattakeaway.com>
2026-02-12 13:24:06 +0200

    feat(01-analytics-01): create analytics module index file
    - Create convex/analytics/index.ts as module entry point
    - Set up structure for future exports:
      - aggregates.ts (TableAggregate instances)
      - events.ts (tracking mutations)
      - queries.ts (dashboard queries)
    - Prepare for incremental analytics implementation

---
+++ convex/analytics/index.ts
@@ -0,0 +1,8 @@
+// Analytics module exports
+// This file exports all analytics-related functions from the convex/analytics directory
+
+// Aggregates will be exported from aggregates.ts (created in next plan)
+// Events will be exported from events.ts (created in next plan)
+// Queries will be exported from queries.ts (created in later plan)
+
+export {};
